668de53fa892bd17d7a50f3f224c1786413b2dd9
sophiaasophieee <sophiaasophieee@gmail.com>
2025-03-03 14:48:50 -0500

    allows user to delete initial file

--- functions/icons.sh
+++ functions/icons.sh
@@ -2,11 +2,11 @@ echo -e ""
 ls /usr/share/applications | grep ".desktop" | grep -v ".hidden" | sed 's/\.desktop//'
 echo -e "Write the application you'd like to change the icon of (EX: code)"
 read -p "> " desktop
-echo -e "Path to the PNG image you'd like to replace the icon with (EX: /home/$USER/Downloads/icon.png)"
+echo -e "Path to the PNG image you'd like to replace the icon with (EX: /home/$USER/Pictures/icon.png)"
 read -p "> " IMAGE
-sudo sed -i "/Icon/c\Icon=${IMAGE}" /usr/share/applications/$desktop.desktop
+sudo cp $IMAGE /usr/share/applications/customicons/$desktop.png
 
-
+sudo sed -i "/Icon/c\Icon=/usr/share/applications/customicons/$desktop.png" /usr/share/applications/$desktop.desktop
 sudo mv /usr/share/applications/$desktop.desktop /tmp/$desktop.desktop
 sleep 10
 sudo mv /tmp/$desktop.desktop /usr/share/applications/$desktop.desktop
