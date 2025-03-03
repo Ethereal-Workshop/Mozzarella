f79a398a19fd997343ac3c50d3d7ad705ff1d3b3
sophiaasophieee <sophiaasophieee@gmail.com>
2025-03-03 14:45:54 -0500

    icons

--- functions/icons.sh
+++ functions/icons.sh
@@ -1,6 +1,13 @@
+echo -e ""
 ls /usr/share/applications | grep ".desktop" | grep -v ".hidden" | sed 's/\.desktop//'
 echo -e "Write the application you'd like to change the icon of (EX: code)"
-read -p "> " -n1 desktop
+read -p "> " desktop
 echo -e "Path to the PNG image you'd like to replace the icon with (EX: /home/$USER/Downloads/icon.png)"
-read -p "> " -n1 IMAGE
-sed -i "s/Icon:[^ ]*/Icon:${IMAGE}/g" yourfile.txt
+read -p "> " IMAGE
+sudo sed -i "/Icon/c\Icon=${IMAGE}" /usr/share/applications/$desktop.desktop
+
+
+sudo mv /usr/share/applications/$desktop.desktop /tmp/$desktop.desktop
+sleep 10
+sudo mv /tmp/$desktop.desktop /usr/share/applications/$desktop.desktop
+echo -e "Done."
