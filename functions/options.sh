4f0256a0a3228f5506c46d02c0e3c2a1dc844d4c
sophiaasophieee <sophiaasophieee@gmail.com>
2025-03-03 18:26:12 -0500

    rootdetect

--- functions/options.sh
+++ functions/options.sh
@@ -2,9 +2,6 @@ while true; do
      clear
      welcome
      echo -e "${COLOR_RED_B}Please select an option or press 'e' to end the script.${COLOR_RESET}\n"
-     if [ "$(id -u)" -eq 0 ]; then
-          echo -e "${COLOR_CYAN_B}Mozzarella is running as root.${COLOR_RESET}\n"
-     fi
      echo -e "${COLOR_RED_B}(E) Exit${COLOR_RESET}"
      echo -e "(1) Change the titlebar color at the top of Linux apps"
      echo -e "(2) Install Steam on Crostini (Can be unstable) [${COLOR_CYAN_B}runs as superuser${COLOR_RESET}]"
--- mozzarella.sh
+++ mozzarella.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 clear
 . functions/common.sh
-. functions/options.sh
+if [ "$(id -u)" -eq 0 ]; then
+    . functions/options.sh
+else
+    [ "$EUID" -ne 0 ] && echo -e "${COLOR_RED_B}Permission Denied. Please re-run Mozzarella as root!${COLOR_RESET}" && read -p "Press enter to exit." && exit
+fi
