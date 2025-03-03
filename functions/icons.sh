b3c230a6d8ae8c1358dfb953f79a39f20353c92f
sophiaasophieee <sophiaasophieee@gmail.com>
2025-03-03 14:53:22 -0500

    persi

--- functions/icons.sh
+++ functions/icons.sh
@@ -2,8 +2,9 @@ echo -e ""
 ls /usr/share/applications | grep ".desktop" | grep -v ".hidden" | sed 's/\.desktop//'
 echo -e "Write the application you'd like to change the icon of (EX: code)"
 read -p "> " desktop
-echo -e "Path to the PNG image you'd like to replace the icon with (EX: /home/$USER/Pictures/icon.png)"
+echo -e "Path to the PNG image you'd like to replace the icon with (EX: /home/$USER/Pictures/icon.png). This file will be stored in the applications directory, you can delete the original file."
 read -p "> " IMAGE
+sudo mkdir -p /usr/share/applications/customicons/
 sudo cp $IMAGE /usr/share/applications/customicons/$desktop.png
 
 sudo sed -i "/Icon/c\Icon=/usr/share/applications/customicons/$desktop.png" /usr/share/applications/$desktop.desktop
