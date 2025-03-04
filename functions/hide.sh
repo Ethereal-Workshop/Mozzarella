0b8dc605b5a8a97061e27a3177522d0865d4e333
sophiaasophieee <sophiaasophieee@gmail.com>
2025-03-03 19:19:27 -0500

    space

--- functions/hide.sh
+++ functions/hide.sh
@@ -7,7 +7,7 @@ echo -e ""
 ls /usr/share/applications | grep ".desktop" | grep -v ".hidden" | sed 's/\.desktop//'
 echo -e ""
 ls /usr/share/applications | grep ".hidden" | sed 's/\.desktop//' | sed 's/\.hidden/ (hidden)/'
-echo -e "Write the application you'd like to hide. This is useful if you want to use an application but don't want the icon. (EX: vim)"
+echo -e "\nWrite the application you'd like to hide. This is useful if you want to use an application but don't want the icon. (EX: vim)"
 echo -e "${COLOR_RED_B}DO NOT INCLUDE THE .hidden!!!${COLOR_RESET}"
 read -p "> " desktop
 if [ ! -f /usr/share/applications/"$desktop".desktop ] && [ ! -f /usr/share/applications/"$desktop".desktop.hidden ]; then
