e564c76576347e24ea26792fd72d2e4b9c096bcf
xmb9 <64978924+EnterTheVoid-x86@users.noreply.github.com>
2025-03-03 19:27:57 -0500

    cleanup

--- mozzarella.sh
+++ mozzarella.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-if [ "$(id -u)" -eq 0 ]; then
-    clear
-    . functions/common.sh
-    . functions/options.sh
-else
-    [ "$EUID" -ne 0 ] && echo -e "\033[1;31mPermission Denied. Please re-run Mozzarella as root!\033[0m" && exit
-fi
+
+[ "$EUID" -ne 0 ] && echo -e "\033[1;31mPlease re-run Mozzarella as root!\033[0m" && exit 1
+
+clear
+. functions/common.sh
+. functions/options.sh
