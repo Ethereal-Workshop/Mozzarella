eba95b635218ed7c94011b28ddd7a26f34553c72
sophiaasophieee <sophiaasophieee@gmail.com>
2025-03-03 19:15:54 -0500

    restore

--- functions/hide.sh
+++ functions/hide.sh
@@ -1,16 +1,25 @@
+#!/bin/bash
+
 clear
 echo -e "${BOLD}Hide or unhide an application${COLOR_RESET}\n"
 
 echo -e ""
-ls /usr/share/applications | grep ".desktop" | grep -v ".hidden"  | sed 's/\.desktop//'
+ls /usr/share/applications | grep ".desktop" | grep -v ".hidden" | sed 's/\.desktop//'
 echo -e ""
 ls /usr/share/applications | grep ".hidden" | sed 's/\.desktop//' | sed 's/\.hidden/ (hidden)/'
 echo -e "Write the application you'd like to hide? This is useful if you want to use an application but don't want the icon. (EX: vim)"
 echo -e "${COLOR_RED_B}DO NOT INCLUDE THE .hidden!!!${COLOR_RESET}"
 read -p "> " desktop
+if [ ! -f /usr/share/applications/"$desktop".desktop ] && [ ! -f /usr/share/applications/"$desktop".desktop.hidden ]; then
+  echo "The file does not exist. Exiting."
+  sleep 0.5
+  return
+fi
+
 echo -e "[1]Hide or [2]Unhide"
 read -p "> " -n1 hide
 case $hide in
      1) mv /usr/share/applications/$desktop.desktop /usr/share/applications/$desktop.desktop.hidden ;;
      2) mv /usr/share/applications/$desktop.desktop.hidden /usr/share/applications/$desktop.desktop ;;
+     *) ;;
 esac
--- functions/icons.sh
+++ functions/icons.sh
@@ -5,14 +5,38 @@ echo -e ""
 ls /usr/share/applications | grep ".desktop" | grep -v ".hidden" | sed 's/\.desktop//'
 echo -e "Write the application you'd like to change the icon of (EX: code)"
 read -p "> " desktop
-echo -e "Path to the PNG image you'd like to replace the icon with (EX: /home/$WHO_USER/Pictures/icon.png). This file will be stored in the applications directory, you can delete the original file."
-read -p "> " IMAGE
+if [ ! -f /usr/share/applications/"$desktop".desktop ]; then
+  echo "The file does not exist. Exiting."
+  sleep 0.5
+  return
+fi
+echo -e "Path to the PNG image you'd like to replace the icon with. (EX: /home/$WHO_USER/Pictures/icon.png). This file will be stored in the applications directory, you can delete the original file."
+echo -e "Or, type 'restore' to restore the original icon"
 mkdir -p /usr/share/applications/customicons/
-cp $IMAGE /usr/share/applications/customicons/$desktop.png
+mkdir -p /usr/share/applications/bak/
+read -p "> " IMAGE
+if test "$IMAGE" != "restore"; then
+    if [ ! -f "$IMAGE" ]; then
+      echo "The file does not exist. Exiting."
+      sleep 0.5
+      return
+    fi
+    rm -f /usr/share/applications/customicons/$desktop.png
+    cp $IMAGE /usr/share/applications/customicons/$desktop.png
 
-sed -i "/Icon/c\Icon=/usr/share/applications/customicons/$desktop.png" /usr/share/applications/$desktop.desktop
-mv /usr/share/applications/$desktop.desktop /tmp/$desktop.desktop
-sleep 10
-mv /tmp/$desktop.desktop /usr/share/applications/$desktop.desktop
+    cp /usr/share/applications/$desktop.desktop /usr/share/applications/bak/$desktop.desktop.bak
+
+    sed -i "/Icon/c\Icon=/usr/share/applications/customicons/$desktop.png" /usr/share/applications/$desktop.desktop
+    mv /usr/share/applications/$desktop.desktop /tmp/$desktop.desktop
+    sleep 5
+    mv /tmp/$desktop.desktop /usr/share/applications/$desktop.desktop
+else
+    mv /usr/share/applications/bak/$desktop.desktop.bak /usr/share/applications/$desktop.desktop
+    mv /usr/share/applications/$desktop.desktop /tmp/$desktop.desktop
+    sleep 5
+    mv /tmp/$desktop.desktop /usr/share/applications/$desktop.desktop
+    rm -f /usr/share/applications/bak/$desktop.desktop.bak
+    rm -f /usr/share/applications/customicons/$desktop.png
+fi
 echo -e "Done."
 desktop=""
